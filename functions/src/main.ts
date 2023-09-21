5193655c4a9092c0ea987a667da6b69e7789854a
Lyukio <lyukio@hotmail.com>
2023-09-21 00:03:48 -0300

    fix: fixing project id when executing tests

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,6 +1,13 @@
 import * as admin from "firebase-admin"
+import * as serviceAccount from "./config/serviceAccount.json"
 
-admin.initializeApp()
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: serviceAccount.project_id,
+        clientEmail: serviceAccount.client_email,
+        privateKey: serviceAccount.private_key,
+    }),
+})
 export const db = admin.firestore()
 db.settings({ timestampsInSnapshots: true })
 export const auth = admin.auth()
--- functions/src/tests/index.spec.ts
+++ functions/src/tests/index.spec.ts
@@ -2,16 +2,27 @@ import { agent as request } from "supertest"
 
 import { main as app } from "../index"
 import { User, UserDocument } from "../models/user"
+// import { Item, ItemDocument } from "../models/item"
 
 describe("order", () => {
+    let user: User
+    // let item: Item
     beforeAll(async () => {
         const userDto: UserDocument = {
             "name": "fake-name",
             "email": "fake@email.com",
             "address": "fake-address",
         }
-        const user = new User(userDto)
+        user = new User(userDto)
         await user.create()
+        // const itemDto: ItemDocument = {
+        //     "qt": 1,
+        //     "price": 2.29,
+        //     "productId": "fake-product-id",
+        //     "categoryIds": ["fake-category-id"],
+        // }
+        // item = new Item(itemDto)
+        // await item.create()
     })
 
     describe("create order", () => {
@@ -19,7 +30,7 @@ describe("order", () => {
             const dto = {
                 "status": "waiting payment",
                 "itemIds": ["9Ifw2cLhVSpPzPVmjSnN"],
-                "userId": "8XVlsSwUbyvdrtm8nNgH",
+                "userId": user.id,
             }
 
             const response = await request(app).post("/orders").send(dto)
